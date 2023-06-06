1673108ada00c87f22481086a60c7f856e557fd2
Lukas Szopo <lukas.szopo@denzelbank.at>
2023-06-06 09:38:22 +0200

    get db files

---
+++ Public/Get-DatabaseDataPath.ps1
@@ -0,0 +1,20 @@
+function Get-DatabaseDataPath {
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] 
+        $dataSource,
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] 
+        $databaseName
+    )
+    if (!(Get-Module "PsLibSqlQueries")) {
+        Import-Module PsLibSqlQueries
+    }
+
+    $query = New-GetDataPathQuery -databaseName $databaseName
+    $queryResult = Invoke-SQLQuery -databaseName $databaseName -dataSource $dataSource -sqlQuery $query 
+    $path = $queryResult[0].physical_name | Split-Path -Parent
+    return $path
+}
---
+++ Public/Get-DatabaseLogPath.ps1
@@ -0,0 +1,20 @@
+function Get-DatabaseLogPath {
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] 
+        $dataSource,
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] 
+        $databaseName
+    )
+    if (!(Get-Module "PsLibSqlQueries")) {
+        Import-Module PsLibSqlQueries
+    }
+
+    $query = New-GetLogPathQuery -databaseName $databaseName
+    $queryResult = Invoke-SQLQuery -databaseName $databaseName -dataSource $dataSource -sqlQuery $query 
+    $path = $queryResult[0].physical_name | Split-Path -Parent
+    return $path
+}
